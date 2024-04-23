5bbf7df64391130320c3c2fb8960c95a34aeb95a
Wellington <wellington.carvalho@cdcscm.com.br>
2024-04-23 06:51:57 -0300

    conflict

--- app/lib/fetchAPI.ts
+++ app/lib/fetchAPI.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import axios from "axios";
 
 export const api = axios.create({
@@ -7,10 +6,3 @@ export const api = axios.create({
     "Content-Type": "application/x-www-form-urlencoded",
   },
 });
-=======
-export const fetchAPI = async <T>(path:String, config?: RequestInit): Promise<T> => {
-    const apiHost:String = (process.env.API_HOST as String) ?? 'http://localhost:3333';
-    const res = await fetch(`${apiHost}${path}`, config);
-    return res.json();
-}
->>>>>>> master
